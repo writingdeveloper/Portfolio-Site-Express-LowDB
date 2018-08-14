55e4bf7a4b1829d2457170ba06572bbe1539620d
sangumee <super2451894@gmail.com>
2018-08-14 20:49:41 +0900

    Temporary Stop
    to study more

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,25 @@
 var express = require('express');
 var router = express.Router();
+var db = require('../lib/db');
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/', function (req, res, next) {
+  db.query('SELECT * FROM main', function (error, results, fields) {
+    if (error) throw error;
+    console.log(JSON.stringify(results));
+    var save =JSON.stringify(results);
+  
+    
+    res.render('index', {
+      title: 'Express',
+      name: results[0].name,
+      imageurl : save.imageurl
+    });
+    console.log(save.imageurl);
+  });
+
+  /* GET home page. */
+
+
 });
 
 module.exports = router;
