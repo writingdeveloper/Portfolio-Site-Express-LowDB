3baa8472bb66109c63f81afe48a1664bbcd93974
sangumee <super2451894@gmail.com>
2018-10-11 23:13:27 +0900

    Update public/javascripts/setting.js
    - Fix Validate Form Option

--- public/javascripts/setting.js
+++ public/javascripts/setting.js
@@ -1,12 +1,15 @@
+/* ---------- Global ---------- */
 
+// Check Delete Option
+function checkDelete() {
+    return confirm('Are you really sure to delete this Data?');
+}
 
+/* ---------- Main PAGE ---------- */
 
-/* ---------- Main PAGE ---------- */ 
+/* ---------- CREATE PAGE ---------- */
 
-
-/* ---------- CREATE PAGE ---------- */ 
-
-// Check Validate Form in Create Page
+// Check Validate Form in Create Page and Update Page
 function validateForm() {
     let projectName = document.getElementById('searchForm').projectName.value;
     let sumLang = document.getElementById('searchForm').sumLang.value;
@@ -14,9 +17,11 @@ function validateForm() {
     let pjdate2 = document.getElementById('searchForm').pjdate2.value;
     let projectExplanation = document.getElementById('searchForm').projectExplanation.value;
     let projectUrl = document.getElementById('searchForm').projectUrl.value;
-    let githuburl = document.getElementById('searchForm').githuburl.value;
+    // let githuburl = document.getElementById('searchForm').githuburl.value;
+    // let imgData = document.getElementById('searchForm').projectImg.value;
+    let portType=document.getElementById('searchForm').portfoliotype.value;
 
-    if (projectName, sumLang, pjdate1, pjdate2, projectExplanation, projectUrl, githuburl === "") {
+    if (projectName, sumLang, pjdate1, pjdate2, projectExplanation, projectUrl, portType === "") {
         alert('Some Fields are Empty!');
         return false;
     }
