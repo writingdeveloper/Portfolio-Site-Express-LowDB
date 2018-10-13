ea6c32532f90b670dab33e322778faf4c878c227
sangumee <super2451894@gmail.com>
2018-10-13 17:20:35 +0900

    Bug FIx
    - File Delete Bug Fix

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
-# Udacity-Build-a-Portfolio-Site-Express
+# Udacity-Build-a-Portfolio-Site-Express  
+[![Build Status](https://travis-ci.org/sangumee/Udacity-Build-a-Portfolio-Site-Express.svg?branch=master)](https://travis-ci.org/sangumee/Udacity-Build-a-Portfolio-Site-Express)
 This repository redeveloped [Link](https://github.com/sangumee/Udacity-Build-a-Portfolio-Site) with NodeJs and Express.
--- routes/index.js
+++ routes/index.js
@@ -2,6 +2,7 @@ let express = require('express');
 let router = express.Router();
 let bodyParser = require('body-parser');
 let path = require('path');
+let fs = require('fs');
 router.use(bodyParser.json()); // to support JSON-encoded bodies
 router.use(bodyParser.urlencoded({ // to support URL-encoded bodies
   extended: true
@@ -9,7 +10,6 @@ router.use(bodyParser.urlencoded({ // to support URL-encoded bodies
 // Parsing Dependency
 var cheerio = require('cheerio');
 var request = require('request');
-
 
 // LowDB Module
 const low = require('lowdb');
@@ -125,11 +125,33 @@ router.post('/create_process', upload.single('projectImg'), function (req, res, 
 router.post('/delete_process/:userId', function (req, res, next) {
   // GET userId
   let id = req.params.userId;
+  // TO GET Image File Information
+  let data = db.get('project').find({
+    id: id
+  }).value();
+  let deleteFileName = data.imgurl;
+  console.log(deleteFileName);
+
   console.log('ITEM : ' + id + 'DELETED!');
   // Delete Process
   db.get('project').remove({
     id: id
   }).write();
+
+  // Prevent Delete Default Image
+  if (deleteFileName === 'app/404.png' || deleteFileName === 'app/Certificate.png' || deleteFileName === 'app/Education.png') {
+    console.log('This DB has no Image, Do nothing with files');
+  } else {
+
+    // Delete Image File
+    fs.unlink('./public/images/' + deleteFileName, function (err) {
+      if (err) {
+        throw err;
+      }
+      console.log(deleteFileName + 'Deleted!')
+    });
+  };
+
   res.redirect('/');
 });
 
@@ -140,7 +162,7 @@ router.get('/update/:pageId', function (req, res) {
   let data = db.get('project').find({
     id: id
   }).value();
-  console.log(data);
+  // console.log(data);
   res.render('update', {
     id: id,
     name: data.name,
@@ -158,14 +180,22 @@ router.get('/update/:pageId', function (req, res) {
 /* POST Update Page */
 router.post('/update_process/:pageId', upload.single('projectImg'), function (req, res) {
   let id = req.params.pageId;
-  console.log(id);
+  // console.log(id);
+  // console.log(req.file);
+  let data = db.get('project').find({
+    id: id
+  }).value();
+  // console.log(data);
+
   console.log(req.file);
 
-  // Check Image Process
-  let checkImg = req.file;
+  let checkImg;
   // If there is no image use 404.png iamge
-  if (checkImg === undefined) {
+  if (data.imgurl === undefined && data.imgurl === 'app/404.png' && req.file === undefined) {
     checkImg = 'app/404.png';
+    console.log(data.imgurl);
+  } else if (req.file === undefined) {
+    checkImg = data.imgurl;
     console.log(checkImg);
   } else {
     checkImg = req.file.originalname;
@@ -200,9 +230,9 @@ router.get('/:pageId', (req, res, next) => {
 
   // Check Image Process
   let checkImg = data.imgurl;
-  let checkType =data.type;
+  let checkType = data.type;
   // Check Image Validate
-  if (checkImg === 'app/404.png' && checkType === 'Certificate') {
+  if (checkImg === 'app/404.png' || checkType === 'Certificate') {
     // Type is Certificate but No Image then USE this link
     checkImg = 'app/Certificate.png';
   }
@@ -242,6 +272,7 @@ router.get('/:pageId', (req, res, next) => {
     res.render('detail', {
       id: data.id,
       dataarray: data,
+      type: data.type,
       name: data.name,
       url: data.url,
       explanation: data.explanation,
